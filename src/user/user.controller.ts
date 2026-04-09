1c34c82648ee88c5d5e3c3a2c8ab3ec2b5679766
ahmalik0311 <ah.malik0311@gmail.com>
2026-04-09 06:18:52 +0500

    3rd  push

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -48,7 +48,7 @@ console.log('Token:', process.env.TWILIO_AUTH_TOKEN);
   }
 
   // Optional: OTP verification
-  @Post('otp/verify')
+  @Post('otp/verify/tester')
   async verifyOtp(@Body() body: { phone: string; otp: string }) {
     // For trial, in-memory or DB checkS
     // Implement verification logic in UserService
